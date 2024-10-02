704225d94d1522d9b4f29449a05e8f39d8767c2e
codeza-ai <darshan.odedara@gmail.com>
2024-10-02 16:42:10 +0530

    Add dummy data that represents a anomaly

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-const pulseData = [ 72, 75, 78, 80, 73, 76, 74, 79];
-const spo2Data= [98, 97, 96, 99, 98, 97, 95, 96];
+const pulseData = [ 72, 75, 78, 80, 73, 76, 74, 79, 20, 72, 120, 45];
+const spo2Data= [98, 97, 96, 99, 98, 97, 95, 96, 97,80, 98, 75];
 let index = 0;
 
 app.get('/', (req, res) => {
@@ -19,6 +20,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     setInterval(() => {
-        index = (index + 1) % 8;
+        index = (index + 1) % 12;
     }, 10000);
 });
